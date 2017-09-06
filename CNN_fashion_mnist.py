60a15e91f97bd20c8d8ab6f4415e439094ee6659
sunny <gsunny4178@gmail.com>
2017-09-06 13:25:24 +0000

    first commit

---
+++ CNN_fashion_mnist.py
@@ -0,0 +1,99 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from keras import backend as K
+from keras import activations
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+from keras.optimizers import SGD, RMSprop, adam
+import fashion_mnist.utils.mnist_reader as mnist_reader
+from vis.visualization import visualize_activation
+from vis.utils import utils
+
+config=tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+batch_size = 400
+num_classes = 10
+epochs = 20
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# the data, shuffled and split between train and test sets
+x_train, y_train = mnist_reader.load_mnist('fashion_mnist/data/fashion', kind='train')
+x_test, y_test = mnist_reader.load_mnist('fashion_mnist/data/fashion', kind='t10k')
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax', name='preds'))
+
+adam = adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=batch_size)
+
+scores = model.evaluate(x_test, y_test, batch_size=batch_size)
+print ("Accuracy: %.2f%%" %(scores[1]*100))
+
+plt.figure(1)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('cnn_fashion_mnist_accuracy.png')
+
+plt.figure(2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.savefig('cnn_fashion_mnist_loss.png')
+
+##layer visualization##
+layer_idx = utils.find_layer_idx(model, 'preds')
+
+model.layers[layer_idx].activation = activations.linear
+model = utils.apply_modifications(model)
+
+for output_idx in np.arange(10):
+    img = visualize_activation(model, layer_idx, filter_indices=output_idx, input_range=(0., 1.))
+    plt.figure()
+    plt.title('Networks perception of {}'.format(output_idx))
+    plt.imsave('cnn_fashion_mnist_vis_' + str(output_idx) + '.png', img[..., 0])
